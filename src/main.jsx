0e8417bc77156224fc16819fac3bea81b280b99c
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-07-11 20:16:11 -0300

    Carrega estilos da nova marca

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
 import './styles/premium.css'
+import './styles/brand-logo.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
