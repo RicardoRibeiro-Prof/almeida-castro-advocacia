ece6c91ce3b358604c4021cb675ddd609ce5603c
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-07-12 08:48:54 -0300

    Adiciona atalho de acessibilidade e aviso demonstrativo

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -2,12 +2,19 @@ import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import WhatsAppButton from '../components/WhatsAppButton'
+import SITE from '../config/site'
 
 export default function PublicLayout() {
   return (
     <div className="site-shell">
+      <a className="skip-link" href="#main-content">Pular para o conteúdo principal</a>
+      {SITE.isDemo && (
+        <div className="demo-banner" role="note">
+          Projeto demonstrativo: escritório, profissionais e dados de contato são fictícios.
+        </div>
+      )}
       <Header />
-      <main id="main-content">
+      <main id="main-content" tabIndex="-1">
         <Outlet />
       </main>
       <Footer />
